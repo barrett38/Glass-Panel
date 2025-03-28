b1d024fdd8fdedfc13e108a576bb93047536ef6e
Barrett Anderson <barrett1614@gmail.com>
2025-03-28 09:20:07 -0500

    Created JSX

---
+++ src/components/GlassPanel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"; // Import CSS for styling
+
+const GlassPanel = () => {
+  return (
+    <div className="glass">
+      <h2>Big Sur Glass Effect</h2>
+      <p>This panel has a soft blur effect.</p>
+    </div>
+  );
+};
+
+export default GlassPanel;
